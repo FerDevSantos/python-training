e39eb69c37ad865769ed00e8df4d4a089ad76529
FerDevSantos <cids82@gmail.com>
2019-08-18 12:02:38 +1000

    training 1

--- Ch2/variables_start.py
+++ Ch2/variables_start.py
@@ -3,13 +3,21 @@
 #
 
 # Declare a variable and initialize it
-
+f=0
+print(f)
 
 # # re-declaring the variable works
+f="abc"
+print(f)
 
-
-# # ERROR: variables of different types cannot be combined
-
+# # ERROR: variables of different types cannot be combined ( remember python is strong typed language )
+print("This is a string " + str(123))
 
 # Global vs. local variables in functions
+def someFunction():
+    global f
+    f="def"
+    print(f)
 
+someFunction()
+print(f)
